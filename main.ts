d68cb86e8a0e4453258837874d5a57253c6caffa
Robert Szaloki <r.szaloki@gmail.com>
2020-11-21 16:34:14 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-basic.showLeds(`
-    . # . # .
-    # . # . #
-    . # . # .
-    . # . # .
-    # . # . #
-    `)
+let megy = 1
 basic.forever(function () {
-	
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 15 && megy == 1) {
+        megy = 0
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 128)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 128)
+        basic.pause(300)
+        maqueen.motorStop(maqueen.Motors.All)
+        megy = 1
+    }
+    if (megy == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    }
 })
